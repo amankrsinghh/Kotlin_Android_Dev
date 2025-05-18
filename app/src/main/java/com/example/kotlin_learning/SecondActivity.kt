adbb559ec12bb3fb27cc3d3578db285f9290474c
xaman <xaman282@gmail.com>
2025-05-18 18:43:07 +0530

    kotlin android practical

--- app/src/main/java/com/example/kotlin_learning/SecondActivity.kt
+++ app/src/main/java/com/example/kotlin_learning/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlin_learning
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,11 +19,14 @@ class SecondActivity : AppCompatActivity() {
         val btnCall: Button = findViewById(R.id.btnCall)
         val btnBrowser: Button = findViewById(R.id.btnBrowser)
         val btnSms: Button = findViewById(R.id.btnSms)
-
+        val btnCamera: Button = findViewById(R.id.btnCamera)
+        val btnPortfolio: Button = findViewById(R.id.btnPortfolio)
 
         // Set click listeners for the buttons
         btnCall.setOnClickListener {
-            // Handle the call button click
+           val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:7033759086")
+            startActivity(intent)
         }
 
         btnBrowser.setOnClickListener {
@@ -30,19 +34,26 @@ class SecondActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW)
                intent.data = Uri.parse("https://www.linkedin.com/in/aman-kumar-singh-210b481a4/")
                startActivity(intent)
-
-
-
         }
 
         btnSms.setOnClickListener {
             // Handle the SMS button click
-            val smsIntent = Intent(Intent.ACTION_VIEW)
+            val smsIntent = Intent(Intent.ACTION_SENDTO)
             smsIntent.data = Uri.parse("sms:7033759086") // Receiver's phone number
             smsIntent.putExtra("sms body", "Hello, this is a test message!") // Message content
             startActivity(smsIntent)
 
         }
+        btnCamera.setOnClickListener {
+            // Handle the camera button click
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(cameraIntent)
+        }
+        btnPortfolio.setOnClickListener {
+            val intent = Intent(this, Web_View::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/kotlin_learning/Web_View.kt
@@ -0,0 +1,36 @@
+package com.example.kotlin_learning
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Web_View : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_web_view)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.safeBrowsingEnabled = true
+        webView.loadUrl("https://amankrsinghh.github.io/Myportfolio/")
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
